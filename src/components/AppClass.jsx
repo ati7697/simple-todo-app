02032647a9819e090ca0bf112df53ebe4901072d
Ati <88265864+ati7697@users.noreply.github.com>
2025-03-28 11:17:10 +1000

    edit and update tasks

--- src/components/AppClass.jsx
+++ src/components/AppClass.jsx
@@ -1,11 +1,10 @@
 import './../App.css'
 import React, { Component } from 'react'
+import { PencilSquareIcon } from "@heroicons/react/24/outline";
 import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
-import { ChevronDownIcon, EllipsisVerticalIcon, XMarkIcon,PlusCircleIcon } from '@heroicons/react/20/solid'
-import {PencilIcon, PencilSquareIcon} from "@heroicons/react/24/outline";
+import { ChevronDownIcon, EllipsisVerticalIcon, XMarkIcon } from '@heroicons/react/20/solid'
 
 export default class AppClass extends Component {
-
     constructor(props) {
         super(props)
 
@@ -18,30 +17,80 @@ export default class AppClass extends Component {
 
         this.submitNewTask = (event) => {
             event.preventDefault();
-                this.setState(prevState => {
-                    const newstate = {
-                        ...prevState,
-                        todo_title: "",
-                        todos: [...prevState.todos, {id: prevState.todos.length + 1, title: prevState.todo_title, completed: false}]
-                    }
+            this.setState(prevState => {
+                const newstate = {
+                    ...prevState,
+                    todo_title: "",
+                    todos: [...prevState.todos, {
+                        id: prevState.todos.length + 1,
+                        title: prevState.todo_title,
+                        completed: false
+                    }]
+                }
 
-                    this.setItemToLocalStorage(newstate)
-                    return newstate
-                })
+                this.setItemToLocalStorage(newstate)
+                return newstate
+            })
         }
-        this.editTask = (id) => {
 
+        this.markAsEditing = (id) => {
+            console.log(id)
+            const updatedTodos = this.state.todos.map(todo => {
+                if (todo.id === id) {
+                    todo.isEditing =  true
+                }
+                return todo
+            })
+
+
+            this.setState(prevState => {
+                const newState = {
+                    ...prevState,
+                    isEditing: true,
+                    todos: updatedTodos
+                }
+
+                this.setItemToLocalStorage(newState)
+
+                return newState
+            })
+
+
+        }
+        this.updateTask = (value, id) => {
             this.setState(prevState => {
                 const updatedTodos = prevState.todos.map(todo => {
                     if (todo.id === id) {
-                        todo.title = prevState.todo_title
+                        todo.title = value
+                        todo.isEditing = false
                     }
                     return todo
                 })
 
                 const newState = {
                     ...prevState,
-                    todo_title: "",
+                    isEditing: false,
+                    todos: updatedTodos
+                }
+
+                this.setItemToLocalStorage(newState)
+
+                return newState
+            })
+        }
+
+        this.cancelEdit = (event, id) => {
+            this.setState(prevState => {
+                const updatedTodos = prevState.todos.map(todo => {
+                    if (todo.id === id) {
+                        todo.isEditing = false
+                    }
+                    return todo
+                })
+
+                const newState = {
+                    ...prevState,
+                    isEditing: false,
                     todos: updatedTodos
                 }
 
@@ -68,7 +117,7 @@ export default class AppClass extends Component {
         }
 
         this.deleteCompletedTasks = () => {
-           const uncompeletedTask = this.state.todos.filter(task => task.completed === false)
+            const uncompeletedTask = this.state.todos.filter(task => task.completed === false)
 
             this.setState(prevState => {
                 const newState = {
@@ -169,10 +218,10 @@ export default class AppClass extends Component {
             const activeTasks = JSON.parse(localStorage.getItem("todos")).filter(task => task.completed === false)
 
             this.setState(prevState => {
-               return {
+                return {
                     ...prevState,
                     todos: activeTasks
-               }
+                }
 
             })
         }
@@ -182,25 +231,23 @@ export default class AppClass extends Component {
         }
 
         this.filterOptions = [
-            { name : "All tasks", action: this.showAllTasks },
-            { name : "Completed tasks", action: this.showCompletedTasks },
-            { name : "Active tasks", action: this.showNotCompletedTasks }
+            {name: "All tasks", action: this.showAllTasks},
+            {name: "Completed tasks", action: this.showCompletedTasks},
+            {name: "Active tasks", action: this.showNotCompletedTasks}
         ]
-
 
 
     }
 
     render() {
-        const { isEditing } = this.state;
         return (
             <div>
-                <div className="flex flex-col md:mx-auto p-6 md:my-64 md:rounded-2xl shadow-xl h-screen md:h-fit md:w-fit gap-2 bg-white">
+                <div className="flex flex-col md:mx-auto p-6 md:my-64 md:rounded-2xl shadow-xl h-screen md:max-w-screen-sm md:max-h-max gap-2 bg-white">
                     <h1 className=" text-xl p-2 font-bold">To do App</h1>
                     <form
-                        action={ "#" }
-                        onSubmit={ this.submitNewTask }
-                        className="w-full max-w-md lg:col-span-5 lg:pt-2">
+                        action={"#"}
+                        onSubmit={this.submitNewTask}
+                        className="w-full max-w-screen-sm lg:col-span-5 lg:pt-2">
                         <div className="flex gap-x-4">
                             <label htmlFor="email-address" className="sr-only">
                                 Email address
@@ -211,8 +258,8 @@ export default class AppClass extends Component {
                                 type="text"
                                 required
                                 placeholder="Enter your task"
-                                value={ this.state.todo_title || "" }
-                                onChange={ (event) => this.setState(prev => ({...prev, todo_title: event.target.value})) }
+                                value={this.state.todo_title || ""}
+                                onChange={(event) => this.setState(prev => ({...prev, todo_title: event.target.value}))}
                                 autoComplete="email"
                                 className="min-w-0 flex-auto rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
                             />
@@ -231,18 +278,18 @@ export default class AppClass extends Component {
                                 id="filter"
                                 name="filter"
                                 aria-label="Filter tasks"
-                                onChange={ (event) => this.filterOptions[event.target.selectedIndex].action() }
+                                onChange={(event) => this.filterOptions[event.target.selectedIndex].action()}
 
                                 className="col-start-1 row-start-1 w-full appearance-none rounded bg-white py-1.5 pl-3 pr-8 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
                             >
-                                { this.filterOptions.map((option, index) => (
-                                  <option
-                                        key={ index }
+                                {this.filterOptions.map((option, index) => (
+                                    <option
+                                        key={index}
                                         className="text-gray-900"
-                                  >
-                                      { option.name }
-                                  </option>
-                                )) }
+                                    >
+                                        {option.name}
+                                    </option>
+                                ))}
                             </select>
                             <ChevronDownIcon
                                 aria-hidden="true"
@@ -256,7 +303,7 @@ export default class AppClass extends Component {
                                     className="flex w-full appearance-none bg-white py-1.5 text-base text-gray-900 sm:text-sm/6"
                                 >
                                     <span className="">Actions</span>
-                                    <EllipsisVerticalIcon className="ml-2 h-5 w-5 text-gray-400"  aria-hidden="true"/>
+                                    <EllipsisVerticalIcon className="ml-2 h-5 w-5 text-gray-400" aria-hidden="true"/>
                                 </MenuButton>
                             </div>
                             <MenuItems
@@ -266,7 +313,7 @@ export default class AppClass extends Component {
                                 <div className="py-1">
                                     <MenuItem>
                                         <button
-                                            onClick={ this.deleteCompletedTasks }
+                                            onClick={this.deleteCompletedTasks}
                                             className="flex w-full justify-between px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900 data-[focus]:outline-none"
                                         >
                                             Remove Completed Tasks
@@ -274,7 +321,7 @@ export default class AppClass extends Component {
                                     </MenuItem>
                                     <MenuItem>
                                         <button
-                                            onClick={ this.removeAllTasks }
+                                            onClick={this.removeAllTasks}
                                             className="flex w-full justify-between px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900 data-[focus]:outline-none"
                                         >
                                             Remove All Tasks
@@ -282,7 +329,7 @@ export default class AppClass extends Component {
                                     </MenuItem>
                                     <MenuItem>
                                         <button
-                                            onClick={ this.checkAllTasks }
+                                            onClick={this.checkAllTasks}
                                             className="flex w-full justify-between px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900 data-[focus]:outline-none"
                                         >
                                             Check All
@@ -290,7 +337,7 @@ export default class AppClass extends Component {
                                     </MenuItem>
                                     <MenuItem>
                                         <button
-                                            onClick={ this.uncheckAllTasks }
+                                            onClick={this.uncheckAllTasks}
                                             className="flex w-full justify-between px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900 data-[focus]:outline-none"
                                         >
                                             Uncheck All
@@ -303,58 +350,72 @@ export default class AppClass extends Component {
 
                     <div>
                         <ul className="space-y-2">
-                            { this.state.todos.map((todo, index) =>
+                            {this.state.todos.map((todo, index) =>
                                 <li
-                                    key={ todo.id }
-                                    className="flex gap-2 text-gray-700 justify-between hover:bg-gray-100 rounded p-2 capitalize"
+                                    key={todo.id}
+                                    className={`flex gap-2 text-gray-700 justify-between rounded p-2 capitalize ${
+                                        todo.isEditing ? '' : 'hover:bg-gray-100'
+                                    }`}
                                 >
-                                    <div className="flex gap-2">
-                                        <div className={ isEditing ?  "hidden" : "flex gap-2" }>
-                                            <input
-                                                checked={ todo.completed }
-                                                onClick={ () => this.completeTask(todo.id) }
-                                                type="checkbox"
-
-                                            />
-                                            <span className={ todo.completed ? "line-through" : "" }>
-                                                { todo.title }
+                                    <div className="flex gap-2 items-center">
+                                        <input
+                                            checked={todo.completed}
+                                            onClick={() => this.completeTask(todo.id)}
+                                            type="checkbox"
+                                        />
+                                        {!todo.isEditing ? (
+                                            <span
+                                                className={ todo.completed ? "line-through" : "" }
+                                                onDoubleClick={() => this.markAsEditing(todo.id)}
+                                            >
+                                                    {todo.title}
                                             </span>
-                                        </div>
-
-                                        <form
-                                            action={ "#" }
-                                            onSubmit={ (event) => this.editTask(todo.id) }
-                                            className={ isEditing ? "flex gap-2" : "hidden" }
-                                        >
-                                            <input
-                                                className={ "hover:bg-gray-100" }
-                                                type={ "text" }
-                                                value={ todo.title }
-                                            />
-                                            <button>
-                                                Save
-                                            </button>
-                                        </form>
-
+                                        ) : (
+                                            <div className="">
+                                                <form
+                                                    action={"#"}
+                                                    onSubmit={(event) => {
+                                                        event.preventDefault();
+                                                        this.updateTask(event.target[0].value, todo.id);
+                                                    }}
+                                                    className="flex gap-2 max-w-md"
+                                                >
+                                                    <input
+                                                        type="text"
+                                                        defaultValue={todo.title}
+                                                        autoFocus
+                                                        className="px-2 py-1"
+                                                    />
+                                                    <button type="submit">
+                                                        Save
+                                                    </button>
+                                                    <button
+                                                        onClick={(event) => {
+                                                            event.preventDefault();
+                                                            this.cancelEdit(event, todo.id);
+                                                        }}
+                                                    >
+                                                        Cancel
+                                                    </button>
+                                                </form>
+                                            </div>
+                                        )}
                                     </div>
-
-                                    <div className="space-x-2">
-                                        <button
-                                            onClick={() => this.setState({ isEditing: true })}
-                                        >
+                                    <div className="flex items-center">
+                                        <button onClick={() => this.markAsEditing(todo.id)}>
                                             <PencilSquareIcon className="size-5 text-gray-500" aria-hidden="true"/>
                                         </button>
-                                        <button onClick={ () => this.deleteTask(todo.id) }>
+                                        <button onClick={() => this.deleteTask(todo.id)}>
                                             <XMarkIcon className="size-5 text-gray-500" aria-hidden="true"/>
                                         </button>
                                     </div>
                                 </li>
-                            ) }
+                            )}
                         </ul>
                     </div>
 
                     <div>
-                    <p className={ this.state.todos.length > 0 ? "hidden" : "text-center text-gray-500" }>
+                        <p className={this.state.todos.length > 0 ? "hidden" : "text-center text-gray-500"}>
                             No tasks available
                         </p>
                     </div>
