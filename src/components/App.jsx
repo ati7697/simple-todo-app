7e45f1fae91ff92fd55d3268033af1550f1591d8
Ati <88265864+ati7697@users.noreply.github.com>
2025-03-28 15:11:51 +1000

    clean up App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,29 +3,7 @@ import { useState } from "react";
 // import AnotherFile from "./anotherFile";
 
 function App() {
-  const [count, setCount] = useState(0);
-  const increment = () => setCount(preCount => preCount += 1);
-  const decrement = () => setCount(preCount => preCount -= 1);
 
-  const [todos, setTodos] = useState([
-    {
-      id: 1,
-      title: "shopping",
-      completed: false
-    },
-    {
-      id: 2,
-      title: "cooking",
-      completed: false
-    },
-    {
-      id: 3,
-      title: "cleaning",
-      completed: false
-    }
-  ]);
-
-    const [newTask, setNewTask] = useState("");
 
   return (
     <div className="App">
